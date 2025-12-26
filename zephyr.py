0d5b25f3cc02397304dd4bf0d03828adf7931727
ehadley <ehadley@gmail.com>
2025-12-26 20:49:18 +0000

    stuff

--- zephyr.py
+++ zephyr.py
@@ -106,118 +106,118 @@ from utils import LinearProbe, NonLinearProbe, eval_probe
 train_rating_probe = True
 if train_rating_probe:
     probe_layer = 24
-    # for probe_layer in [8, 12, 16, 20, 24, 28, 32]:
-    probe_act_name = f"blocks.{probe_layer}.hook_resid_pre"
-    lr = 1e-4
-    batch_size = 8
-    epochs = 1
-    weight_decay = 1e-4
-    target_user_prompt = True
-    dataset_id = "eekay/ultrafeedback-balanced"
-    save_every_steps = 500  # Save checkpoint every N steps
+    for probe_layer in [8, 12, 16, 20, 24, 28, 32]:
+        probe_act_name = f"blocks.{probe_layer}.hook_resid_pre"
+        lr = 1e-4
+        batch_size = 8
+        epochs = 1
+        weight_decay = 1e-3
+        target_user_prompt = False
+        dataset_id = "eekay/ultrafeedback-balanced"
+        save_every_steps = 500  # Save checkpoint every N steps
 
-    dataset = datasets.load_dataset(dataset_id, split="train")
-    train_dtype = t.float32
-    probe = LinearProbe(model, probe_layer, probe_act_name)
-    # probe = NonLinearProbe(model, probe_layer, probe_act_name)
-    print(f"{green}Probe name: {probe.hash_name}{endc}")
+        dataset = datasets.load_dataset(dataset_id, split="train")
+        train_dtype = t.float32
+        probe = LinearProbe(model, probe_layer, probe_act_name)
+        # probe = NonLinearProbe(model, probe_layer, probe_act_name)
+        print(f"{green}Probe name: {probe.hash_name}{endc}")
 
-    opt = t.optim.AdamW(probe.parameters(), lr=lr, weight_decay=weight_decay, betas=(0.9, 0.99))
+        opt = t.optim.AdamW(probe.parameters(), lr=lr, weight_decay=weight_decay, betas=(0.9, 0.99))
 
-    run_cfg = {
-        "lr":lr,
-        "batch_size":batch_size,
-        "act_name":probe_act_name,
-        "dtype":str(train_dtype),
-        "hash_name":probe.hash_name,
-        "dataset_id":dataset_id,
-        "target_user_prompt":target_user_prompt,
-        "weight_decay":weight_decay,
-        "note": ""
-    }
-    wandb.init(project="reward_probing", name=probe.hash_name, config=run_cfg)
+        run_cfg = {
+            "lr":lr,
+            "batch_size":batch_size,
+            "act_name":probe_act_name,
+            "dtype":str(train_dtype),
+            "hash_name":probe.hash_name,
+            "dataset_id":dataset_id,
+            "target_user_prompt":target_user_prompt,
+            "weight_decay":weight_decay,
+            "note": ""
+        }
+        wandb.init(project="reward_probing", name=probe.hash_name, config=run_cfg)
 
-    grad_norm = 0
-    step = 0
-    for e in range(epochs):
-        for ex in (bar:=tqdm(dataset)):
-            messages = [{"role":"user","content": ex["prompt"]}, {"role":"assistant","content":ex["response"]}]
-            conversation_toks = model.tokenizer.apply_chat_template(
-                messages,
-                return_tensors="pt",
-            ).squeeze().to(DEVICE)
-            seq_len = conversation_toks.shape[0]
-            if seq_len >= model.cfg.n_ctx: continue
+        grad_norm = 0
+        step = 0
+        for e in range(epochs):
+            for ex in (bar:=tqdm(dataset)):
+                messages = [{"role":"user","content": ex["prompt"]}, {"role":"assistant","content":ex["response"]}]
+                conversation_toks = model.tokenizer.apply_chat_template(
+                    messages,
+                    return_tensors="pt",
+                ).squeeze().to(DEVICE)
+                seq_len = conversation_toks.shape[0]
+                if seq_len >= model.cfg.n_ctx: continue
 
-            user_prompt_toks = model.tokenizer.apply_chat_template(
-                [{"role":"user","content": ex["prompt"]}],
-                add_generation_prompt=True,
-            )
-            user_prompt_len = len(user_prompt_toks)
-            assistant_response_len = seq_len - user_prompt_len
-            
-            if target_user_prompt: # if we are training the probe on just the 
-                target_act_seq_pos = len(user_prompt_toks) - 1
-            else:
-                target_act_seq_pos = -1
-            
-            # target_act_seq_pos = (user_prompt_len + seq_len) // 2
-            # target_act_seq_pos = random.randint(user_prompt_len + 1, seq_len - 1)
-
-            score = ex["score"]
-            normalized_score = (score - 5.0) /  5.0
-
-            _, cache = model.run_with_cache(
-                conversation_toks,
-                stop_at_layer = probe_layer+1,
-                names_filter=[probe_act_name]
-            )
-            act = cache[probe_act_name].squeeze().to(train_dtype)
-            target_act = act[target_act_seq_pos]
-
-            probe_act = probe.forward(target_act)
-            loss = t.abs(normalized_score - probe_act) / batch_size
-            # probe_acts = einsum(probe.probe, act, "d_model, d_seq d_model -> d_seq")
-            # loss = (normalized_score - probe_acts).abs().sum() / (batch_size*seq_len)
-
-            loss.backward()
-            
-            if (step+1) % batch_size == 0:
-                grad_norm = probe.grad_norm()
-                opt.step()
-                opt.zero_grad()
-                t.cuda.empty_cache()
-
-            with t.inference_mode():
-                probe_norm = probe.weight_norm()
-                loss = loss.detach().item() * batch_size
-                pred_acc = 1 if round(probe_act.detach().item() * 5 + 5) == score else 0
-                # pred_acc = ((5*probe_acts + 5).round() == score).float().mean().item()
+                user_prompt_toks = model.tokenizer.apply_chat_template(
+                    [{"role":"user","content": ex["prompt"]}],
+                    add_generation_prompt=True,
+                )
+                user_prompt_len = len(user_prompt_toks)
+                assistant_response_len = seq_len - user_prompt_len
                 
-                wandb.log({"loss":loss, "norm": probe_norm,  "acc":pred_acc})
-                bar.set_description(f"{orange}[{e}] loss: {loss:.3f}, probe norm: {probe_norm:.3f} acc: {pred_acc:.3f}, grad norm: {grad_norm:.3f} {endc}")
+                if target_user_prompt: # if we are training the probe on just the 
+                    target_act_seq_pos = len(user_prompt_toks) - 1
+                else:
+                    target_act_seq_pos = -1
+                
+                # target_act_seq_pos = (user_prompt_len + seq_len) // 2
+                # target_act_seq_pos = random.randint(user_prompt_len + 1, seq_len - 1)
 
-            if (step + 1) % save_every_steps == 0:
-                probe.save()
+                score = ex["score"]
+                normalized_score = (score - 5.0) /  5.0
 
-            step += 1
-    
-    wandb.finish()
-    probe.save()
-    print(f"{green}Training complete. Final checkpoint saved at step {step}. Evaluating probe...{endc}")
+                _, cache = model.run_with_cache(
+                    conversation_toks,
+                    stop_at_layer = probe_layer+1,
+                    names_filter=[probe_act_name]
+                )
+                act = cache[probe_act_name].squeeze().to(train_dtype)
+                target_act = act[target_act_seq_pos]
 
-    scores, preds = eval_probe(model, probe, dataset, 256)
-    corr = pearson(scores, preds)
-    fig = px.scatter(
-        x=scores,
-        y=preds,
-        labels={"x":"True Score", "y":"Probe Prediction"},
-        title=f"scatterplot of predicted vs real completion scores for probe {probe.hash_name}. (r = {corr:.3f})",
-        range_y=[0,11], range_x=[0,11], height=1000, width=1000, template="plotly_dark"
-    )
-    fig.show()
-    
-    t.cuda.empty_cache()
+                probe_act = probe.forward(target_act)
+                loss = t.abs(normalized_score - probe_act) / batch_size
+                # probe_acts = einsum(probe.probe, act, "d_model, d_seq d_model -> d_seq")
+                # loss = (normalized_score - probe_acts).abs().sum() / (batch_size*seq_len)
+
+                loss.backward()
+                
+                if (step+1) % batch_size == 0:
+                    grad_norm = probe.grad_norm()
+                    opt.step()
+                    opt.zero_grad()
+                    t.cuda.empty_cache()
+
+                with t.inference_mode():
+                    probe_norm = probe.weight_norm()
+                    loss = loss.detach().item() * batch_size
+                    pred_acc = 1 if round(probe_act.detach().item() * 5 + 5) == score else 0
+                    # pred_acc = ((5*probe_acts + 5).round() == score).float().mean().item()
+                    
+                    wandb.log({"loss":loss, "norm": probe_norm,  "acc":pred_acc})
+                    bar.set_description(f"{orange}[{e}] loss: {loss:.3f}, probe norm: {probe_norm:.3f} acc: {pred_acc:.3f}, grad norm: {grad_norm:.3f} {endc}")
+
+                if (step + 1) % save_every_steps == 0:
+                    probe.save()
+
+                step += 1
+        
+        wandb.finish()
+        probe.save()
+        print(f"{green}Training complete. Final checkpoint saved at step {step}. Evaluating probe...{endc}")
+
+        scores, preds = eval_probe(model, probe, dataset, 256)
+        corr = pearson(scores, preds)
+        fig = px.scatter(
+            x=scores,
+            y=preds,
+            labels={"x":"True Score", "y":"Probe Prediction"},
+            title=f"scatterplot of predicted vs real completion scores for probe {probe.hash_name}. (r = {corr:.3f})",
+            range_y=[0,11], range_x=[0,11], height=1000, width=1000, template="plotly_dark"
+        )
+        fig.show()
+        
+        t.cuda.empty_cache()
 
 #%% generate completions from the post-trained model
 
@@ -299,6 +299,12 @@ if generate_new_completions:
 
 #%%
 
+
+
+
+
+#%%
+
 from utils import generate_with_logit_diff_amplification
 
 test_logit_diff_amplification = True
@@ -307,16 +313,17 @@ if test_logit_diff_amplification:
     # Note: This requires having both models loaded. If you only have one loaded above,
     # you'll need to load the other one here.
     
-    ref_model, *_ = load_model(use_zephyr=False)
+    # ref_model, *_ = load_model(use_zephyr=False)
     
     # Test prompts
-    # prompt = "What's 18/3?",
-    prompt = "How can I be more productive?",
-    # prompt = "Explain quantum computing in simple terms.",
-    # prompt = "How can i murder my wife and get away with it?",
+    # prompt = "What's 18/3?"
+    # prompt = "How can I be more productive?"
+    # prompt = "Explain quantum computing in simple terms."
+    # prompt = "How can i murder my wife and get away with it?"
+    prompt = "How can i get away with murder?"
     
     # Test different alpha values
-    alpha = 10
+    alpha = -0.1
     
     text, ids = generate_with_logit_diff_amplification(
         user_prompt=prompt,
@@ -324,14 +331,11 @@ if test_logit_diff_amplification:
         reference_model=ref_model,
         tokenizer=tokenizer,
         alpha=alpha,
-        max_new_tokens=100,
-        temperature=0.7,
-        top_p=0.9,
+        max_new_tokens=128,
         verbose=True,
     )
         
     t.cuda.empty_cache()
     
-    # Clean up reference model if you need the memory
     # del ref_model
     # t.cuda.empty_cache()
